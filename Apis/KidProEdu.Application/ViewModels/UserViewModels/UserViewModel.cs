64fdf91942b4c107a7e7d8e35d772ebaf481590f
neil.duynguyen <neil.duynguyen@gmail.com>
2024-02-07 12:18:58 +0700

    Update UserViewModel

--- Apis/KidProEdu.Application/ViewModels/UserViewModels/UserViewModel.cs
+++ Apis/KidProEdu.Application/ViewModels/UserViewModels/UserViewModel.cs
@@ -8,6 +8,7 @@ namespace KidProEdu.Application.ViewModels.UserViewModels
 {
     public class UserViewModel
     {
+        public Guid Id { get; set; }
         public string UserName { get; set; }
         public string FullName { get; set; }
 
