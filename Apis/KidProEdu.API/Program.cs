bd4550571440a932ef0373e7e32406ffd6f1c0d7
Nghĩa Phạm (Darius Pham) <nghia01694049627@gmail.com>
2024-02-29 19:13:22 +0700

    update CORS server

--- Apis/KidProEdu.API/Program.cs
+++ Apis/KidProEdu.API/Program.cs
@@ -38,7 +38,9 @@ namespace KidProEdu.API
                 options.AddPolicy(name: MyAllowSpecificOrigins,
                                   policy =>
                                   {
-                                      policy.WithOrigins("http://localhost:5173", "http://127.0.0.1:5173"
+                                      policy.WithOrigins("http://localhost:5173",
+                                                        "http://127.0.0.1:5173",
+                                                        "https://kid-pro-edu.netlify.app"
                                                           )
                                                             //.AllowAnyOrigin()
                                                             .AllowAnyHeader()
