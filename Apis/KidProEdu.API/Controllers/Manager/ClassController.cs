ef05e2485c8f71e8e2b4b0f4de85c271e1c9fc72
Nghĩa Phạm (Darius Pham) <nghia01694049627@gmail.com>
2024-03-04 13:13:28 +0700

    add api GetClassBySemester

--- Apis/KidProEdu.API/Controllers/Manager/ClassController.cs
+++ Apis/KidProEdu.API/Controllers/Manager/ClassController.cs
@@ -32,6 +32,18 @@ namespace KidProEdu.API.Controllers.Manager
             }
             return Ok(Class);
         }
+        
+        [HttpGet("GetClassBySemester/{id}")]
+        /*[Authorize(Roles = ("Admin"))]*/
+        public async Task<IActionResult> GetClassBySemester(Guid id)
+        {
+            var Class = await _classService.GetClassBySemester(id);
+            if (Class == null)
+            {
+                return NotFound();
+            }
+            return Ok(Class);
+        }
 
         [HttpPost]
         /*[Authorize(Roles = ("Admin"))]*/
