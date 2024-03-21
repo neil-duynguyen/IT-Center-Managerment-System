27ef50aa377070b8fa97d5b050321298bc14be10
Nghĩa Phạm (Darius Pham) <86977688+do-notgiveup@users.noreply.github.com>
2024-03-21 14:44:37 +0700

    fix api class details

--- Apis/KidProEdu.Application/ViewModels/AdviseRequestViewModels/UpdateAdviseRequestViewModel.cs
+++ Apis/KidProEdu.Application/ViewModels/AdviseRequestViewModels/UpdateAdviseRequestViewModel.cs
@@ -10,6 +10,7 @@ namespace KidProEdu.Application.ViewModels.AdviseRequestViewModels
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
+        public DateTime TestDate { get; set; }
         public string Location { get; set; }
         public bool? IsTested { get; set; }
         public StatusAdviseRequest StatusAdviseRequest { get; set; }
