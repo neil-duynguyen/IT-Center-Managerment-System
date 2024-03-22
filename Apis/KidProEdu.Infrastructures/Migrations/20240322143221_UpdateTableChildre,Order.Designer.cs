02b06e55bc45084fb261c621163348b5cc71b658
neil.duynguyen <neil.duynguyen@gmail.com>
2024-03-22 21:56:33 +0700

    UpdateDBTableChildren,Order and feature GetChildrenByParentId

--- Apis/KidProEdu.Infrastructures/Migrations/20240322143221_UpdateTableChildre,Order.Designer.cs
+++ Apis/KidProEdu.Infrastructures/Migrations/20240322143221_UpdateTableChildre,Order.Designer.cs
@@ -4,6 +4,7 @@ using KidProEdu.Infrastructures;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KidProEdu.Infrastructures.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240322143221_UpdateTableChildre,Order")]
+    partial class UpdateTableChildreOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
