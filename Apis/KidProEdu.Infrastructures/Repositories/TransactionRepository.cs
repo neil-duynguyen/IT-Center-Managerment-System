7999dd0fd8f06c79675cf2b1b640518e04e006bb
Nguyễn Văn Duy <113020285+neil-duynguyen@users.noreply.github.com>
2024-03-26 13:37:55 +0700

    Feature GetAllTransaction, GetTransactionDetailByTransactionId

--- Apis/KidProEdu.Infrastructures/Repositories/TransactionRepository.cs
+++ Apis/KidProEdu.Infrastructures/Repositories/TransactionRepository.cs
@@ -2,6 +2,7 @@
 using KidProEdu.Application.Interfaces;
 using KidProEdu.Application.IRepositories;
 using KidProEdu.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +13,14 @@ namespace KidProEdu.Infrastructures.Repositories
 {
     public class TransactionRepository : GenericRepository<Transaction>, ITransactionRepository
     {
-        public TransactionRepository(AppDbContext context, ICurrentTime timeService, IClaimsService claimsService) : base(context, timeService, claimsService)
+        private readonly AppDbContext _dbContext;
+        public TransactionRepository(AppDbContext dbContext, ICurrentTime timeService, IClaimsService claimsService) : base(dbContext, timeService, claimsService)
         {
+            _dbContext = dbContext;
+        }
+        public override async Task<List<Transaction>> GetAllAsync()
+        {
+            return await _dbContext.Transaction.Include(x => x.OrderDetail).ThenInclude(x => x.Order).ToListAsync();
         }
     }
 }
