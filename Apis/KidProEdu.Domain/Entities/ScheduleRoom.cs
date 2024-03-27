473446e4a939e2479604e5f652c4fc561ea12203
Nghĩa Phạm (Darius Pham) <nghia01694049627@gmail.com>
2024-03-27 22:02:57 +0700

    update UpdateQuestionViewModel

--- Apis/KidProEdu.Domain/Entities/ScheduleRoom.cs
+++ Apis/KidProEdu.Domain/Entities/ScheduleRoom.cs
@@ -14,8 +14,8 @@ namespace KidProEdu.Domain.Entities
         public Guid? RoomId { get; set; }
         [ForeignKey("Schedule")]
         public Guid? ScheduleId { get; set; }
-        public DateTime? StartDate { get; set; } //ngày mà lịch đó đổi phòng học tạm thời 1 hôm
-        public DateTime? EndDate { get; set; } //ngày mà lịch đó đổi phòng học tạm thời 1 hôm
+        public DateTime? StartDate { get; set; } 
+        public DateTime? EndDate { get; set; } 
         public ScheduleRoomStatus? Status { get; set; }
         public virtual Room? Room { get; set; }
         public virtual Schedule? Schedule { get; set; }
