10301d56e971aeef2ce2e5a1c0228ede6c889a50
Tobi.Vannguyen <trjace21222@gmail.com>
2024-03-31 00:04:14 +0700

    migration status children profile

---
+++ Apis/KidProEdu.Infrastructures/Migrations/20240330170113_StatusChildrenProfile.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KidProEdu.Infrastructures.Migrations
+{
+    /// <inheritdoc />
+    public partial class StatusChildrenProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "ChildrenProfile",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ChildrenProfile");
+        }
+    }
+}
