b53bb704853118527ce4fdf361a6516bd8c53d90
Nghĩa Phạm (Darius Pham) <nghia01694049627@gmail.com>
2024-04-04 00:21:15 +0700

    Update ScheduleController.cs

--- Apis/KidProEdu.API/Controllers/Manager/ScheduleController.cs
+++ Apis/KidProEdu.API/Controllers/Manager/ScheduleController.cs
@@ -44,7 +44,7 @@ namespace KidProEdu.API.Controllers.Manager
             try
             {
                 var model = await _scheduleService.CreateAutomaticalySchedule();
-                if (!model.IsNullOrEmpty())
+                /*if (!model.IsNullOrEmpty())
                 {
                     return Ok("Còn " + model.FirstOrDefault().CountSchedule + " lớp chưa được xếp giáo viên \n" +
                               "Còn " + model.FirstOrDefault().CountRoom + " lớp chưa được xếp phòng");
@@ -52,7 +52,8 @@ namespace KidProEdu.API.Controllers.Manager
                 else
                 {
                     return Ok("Xếp lịch thành công");
-                }
+                }*/
+                return Ok(model);
             }
             catch (Exception ex)
             {
