bd056bbdc982cfe19f73d826df0d37c40b2b2f41
Tobi.Vannguyen <trjace21222@gmail.com>
2024-04-17 21:47:55 +0700

    fix children aswer null

--- Apis/KidProEdu.Application/Validations/ChildrenAnswers/CreateChildrenAnswerViewModelValidator.cs
+++ Apis/KidProEdu.Application/Validations/ChildrenAnswers/CreateChildrenAnswerViewModelValidator.cs
@@ -18,7 +18,6 @@ namespace KidProEdu.Application.Validations.ChildrenAnswers
             RuleFor(x => x.ChildrenProfileId).NotEmpty().WithMessage("Nguời dùng không được để trống.");
             RuleFor(x => x.ExamId).NotEmpty().WithMessage("Bài kiểm tra không được để trống.");
             RuleFor(x => x.QuestionId).NotEmpty().WithMessage("Câu hỏi không được để trống.");
-            RuleFor(x => x.Answer).NotEmpty().WithMessage("Câu trả lời không được để trống.");
             RuleFor(x => x.ScorePerQuestion)
                 .GreaterThanOrEqualTo(0).WithMessage("Điểm phải lớn hơn hoặc bằng 0.");
         }
