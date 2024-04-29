3fdf29a17fa04d4994f16c3285dbca9c4c2e9e40
Nghĩa Phạm (Darius Pham) <nghia01694049627@gmail.com>
2024-04-29 11:10:16 +0700

    add api getAdviseRequestByUserId

--- Apis/KidProEdu.Application/Services/AdviseRequestService.cs
+++ Apis/KidProEdu.Application/Services/AdviseRequestService.cs
@@ -267,5 +267,16 @@ namespace KidProEdu.Application.Services
                          "</body></html>");
             }
         }
+
+        public async Task<List<AdviseRequestViewModel>> GetAdviseRequestByUserId(Guid id)
+        {
+            var adviseRequest = await _unitOfWork.AdviseRequestRepository.GetAdviseRequestByUserId(id);
+            foreach (var item in adviseRequest)
+            {
+                var mapper = _mapper.Map<AdviseRequest>(item);
+
+            }
+            return _mapper.Map<List<AdviseRequestViewModel>>(adviseRequest);
+        }
     }
 }
